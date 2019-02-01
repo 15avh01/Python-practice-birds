a6e4a4d80ef0c942d8191a9a32b8dda9136bea65
Alyse <alyseheaston@ou.edu>
2019-01-31 23:18:07 -0600

    Update fakebird_csv_manipulation.py
    Updated code to perform list comparisons without sets while also allowing for matching within a range. Made fixes so it writes to csv files correctly.

--- fakebird_csv_manipulation.py
+++ fakebird_csv_manipulation.py
@@ -9,9 +9,9 @@ Created on Thu Nov 29 14:44:44 2018
 import csv
 import math
 
-with open("exportedcsv.csv", "w") as newfile:
-    mystring = (first only" + "," + "second only" + "," + "area" + "\n"
-    file.write(mystring)
+with open("FirstOnly.csv", "w") as newfile:
+    mystring = ("In First List Only" + "\n")
+    newfile.write(mystring)
 
     f = open("fakebirds1.csv")
     csv_f = csv.reader(f)
@@ -23,51 +23,86 @@ with open("exportedcsv.csv", "w") as newfile:
     #set up empty lists to fill with data
 
     for column in csv_f:
-        birdlist.append(column[0])
         timelist.append(column[1])
         radiuslist.append(column[2])
         #creates list with data for each column
-
+	timelist = timelist [1:]
+	#removes header
+	
     radiuslist = radiuslist[1:]
     radiuslist = [float(x) for x in radiuslist]
     #converts list to floats so operations can be performed 
-
-    f = open("fakebirds2.csv")
-    csv_f = csv.reader(f)
-
-    birdlist2 = []
-    timelist2 = []
-    for column in csv_f:
-        birdlist2.append(column[0])
-        timelist2.append(column[1])
-    #creates lists for second file
-
-    settime = set(timelist)
-    settime2 = set(timelist2)
-
-    firstonly = settime - settime2
-    secondonly = settime2 - settime
-    #finds differences between the lists
-
     area = [x**2 for x in radiuslist]
     area = [x*math.pi for x in area]
     #calculates estimated area of birds from radii
 
-    firstonly = list(firstonly)
-    secondonly = list(secondonly)
-    #converts sets back to lists
-    firstonly.sort(key = float)
-    secondonly.sort(key = float)
-    #puts times back in numberical order
+    ff = open("fakebirds2.csv")
+    csv_f = csv.reader(ff)
+    timelist2 = []
 
-with open("exportedcsv.csv", "a") as newfile:
-    mystring=(str(firstonly) + "\n")
-    newfile.write(mystring)
+    for column in csv_ff:
+        timelist2.append(column[1])
+    #creates lists for second file
+	timelist2 = timelist2[1:]
+	#removes header
 
-with open("exportedcsv.csv", "a") as newfile:
-    mystring=(str(secondonly) + "\n")
-    newfile.write(mystring)
+	timelist = [float(x) for x in timelist]
+	timelist2 = [float(x) for x in timelist2]
+	#converts items in both lists from strings to floats
+	
+	mydict = {}
+	zipped = (zip(timelist,area))
+	for x,i in zipped:
+		mydict.setdefault(x, []).append(i)
+		#creates a dictionary to pair times in first list with areas, allowing for multiple values per key
+		
+	firstonly = []
+	secondonly = []
+	
+	for x in timelist:
+		if x not in timelist2:
+			for i in range(0,len(timelist2)):
+				if abs(x - timelist[i]) <= 0.05:
+					pass
+				else:
+					if x not in firstonly:
+						writer = csv.writer(newfile)
+						writer.writerow([x])
+						firstonly.append(x)
+					else:
+						pass
+		else:
+			pass
+	#compares items in timelist with each in timelist2
+	#if the value is in 2, it passes it
+	#If they are within 0.05 of each other, they are counted as equal are passed
+	#If the values are not equal or within 0.05, they are written to file and list
 
-with open("exportedcsv.csv", "a") as newfile:
-    mystring=(str(area) + "\n")
-    newfile.write(mystring)
+with open("SecondOnly.csv", "w") as secondfile:
+	mystring = ("In Second List Only" + "\n")
+	secondfile.write(mystring)
+with open ("SecondOnly.csv", "a") as secondfile:
+	for x in timelist2:
+		if x not in timelist:
+			for i in range(0,len(timelist2)):
+				if abs(x - timelist2[i]) <= 0.05:
+					pass
+				else:
+					if x not in secondonly:
+						writer = csv.writer(secondfile)
+						writer.writerow([x])
+						secondonly.append(x)
+					else:
+						pass
+		else:
+			pass
+	#comparable to list comparison done above but reversed
+	#EXCEPT length in range is matched to shortest list
+	
+with open("Areas.csv", "w") as thirdfile:
+	mystring = ("Areas" + "\n")
+	thirdfile.write(mystring)
+with open("Areas.csv", "a") as thirdfile:
+	for x in area:
+		writer = csv.writer(thirdfile)
+		writer.writerow([x])
